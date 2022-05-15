f979559f5a01218ff850dd8c033e5c749f29eca1
zhangyunnian <zhangyunnian@bytedance.com>
2022-05-15 11:30:37 +0800

    modify

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text className='c:ff0000'>Hello world!</Text>
+      </View>
+    )
+  }
+}
