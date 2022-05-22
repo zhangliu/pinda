b019dc9843db59c8ddaafe874ef3fd99bf794382
zhangyunnian <zhangyunnian@bytedance.com>
2022-05-22 15:55:46 +0800

    modify

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,5 @@
-import { Component } from 'react'
 import 'taro-ui/dist/style/index.scss'
 
-class App extends Component {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
-  }
+export default (props) => {
+  return props.children
 }
-
-export default App
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,6 +3,7 @@ import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 import SinglePosts from '../../components/singlePosts'
+// import GetUserButton from '../../components/getUserButton'
 
 export default () => {
   const [current, setCurrent] = useState(0)
@@ -24,6 +25,7 @@ export default () => {
     // <AtTabs current={current} tabList={tabList} onClick={setCurrent}>
     //   <AtTabsPane current={current} index={0} >
         <View className='ml:32 mr:32 pt:32'>
+          {/* <GetUserButton title='分享' /> */}
           <SinglePosts />
         </View>
     //   </AtTabsPane>
