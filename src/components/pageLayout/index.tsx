334c58764d9093ab590dbbef4fb9cb2afb8cbe17
张云念 <zhangyunnian@bytedance.com>
2024-06-23 21:50:16 +0800

    fix: modify

--- src/components/pageLayout/index.tsx
+++ src/components/pageLayout/index.tsx
@@ -1,13 +1,6 @@
 import { View } from '@tarojs/components';
-import useUser from 'src/hooks/useUser';
-import Loading from '../loading';
 
 export default (props) => {
-  const [user] = useUser();
-
-  if (!user) {
-    return <Loading className='mt:120' />;
-  }
   return (
     <View className='bgc:f7f7f7 h:100%' style={{minHeight: '100vh'}}>
       {props.children}
